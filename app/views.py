3dab1ad74dc5b52cb530f9b66911b0254e71e68e
JonathanSouth <jonathan.south@mymona.uwi.edu>
2018-02-08 04:45:16 +0000

    2018.02.06 Commit

--- app/views.py
+++ app/views.py
@@ -5,6 +5,7 @@ Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
 This file creates your application.
 """
 
+import datetime
 from app import app
 from flask import render_template, request, redirect, url_for, flash
 
@@ -23,6 +24,10 @@ def home():
 def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
+    
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')    
 
 
 ###
@@ -52,6 +57,10 @@ def add_header(response):
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
+    
+    
+def format_date_joined():
+    print "Joined" + date_joined.strftime("%B, %Y")
 
 
 if __name__ == '__main__':
